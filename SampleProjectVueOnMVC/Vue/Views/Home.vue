b69570fd3934b14040b2eeccdea20e1f640357f1
Luis Santiago <luis.satiago56@upr.edu>
2021-02-25 22:02:54 -0700

    Last working changes

--- SampleProjectVueOnMVC/Vue/Views/Home.vue
+++ SampleProjectVueOnMVC/Vue/Views/Home.vue
@@ -1,46 +1,6 @@
 ﻿<template>
     <div class="sampleDiv">
-        <b-row>
-            <b-col>
-                <b-card title="Card Title"
-                        img-src="https://picsum.photos/600/300/?image=25"
-                        img-alt="Image"
-                        img-top
-                        tag="article">
-                    <b-card-text>
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
-                    </b-card-text>
-
-                    <b-button href="#" variant="primary">Go somewhere</b-button>
-                </b-card>
-            </b-col>
-            <b-col>
-                <b-card title="Card Title"
-                        img-src="https://picsum.photos/600/300/?image=25"
-                        img-alt="Image"
-                        img-top
-                        tag="article">
-                    <b-card-text>
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
-                    </b-card-text>
-
-                    <b-button href="#" variant="primary">Go somewhere</b-button>
-                </b-card>
-            </b-col>
-            <b-col>
-                <b-card title="Card Title"
-                        img-src="https://picsum.photos/600/300/?image=25"
-                        img-alt="Image"
-                        img-top
-                        tag="article">
-                    <b-card-text>
-                        Some quick example text to build on the card title and make up the bulk of the card's content.
-                    </b-card-text>
-
-                    <b-button href="#" variant="primary">Go somewhere</b-button>
-                </b-card>
-            </b-col>
-        </b-row>
+        <h1>Hello App</h1>
     </div>
 </template>
 <style>
